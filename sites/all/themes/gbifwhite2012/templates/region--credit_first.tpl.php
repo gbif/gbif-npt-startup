8114a88f17113a5638fb3f6216cf400eff4e350a
Burke Chih-Jen Ko <burkeker@gmail.com>
2012-07-19 17:07:38 +0200

    Provided toggle switch for links in the footer and credit.

--- sites/all/themes/gbifwhite2012/templates/region--credit_first.tpl.php
+++ sites/all/themes/gbifwhite2012/templates/region--credit_first.tpl.php
@@ -1,6 +1,8 @@
 <aside<?php print $attributes; ?>>
   <div<?php print $content_attributes; ?>>
-    <p>&copy;2012 <a href="www.gbif.org">Global Biodiversity Information Facility</a>. Data publishers retain all rights to data.</p>
+    <?php if (theme_get_setting('toggle_gbif_credit')): ?>
+      <p>&copy;2012 <a href="www.gbif.org">Global Biodiversity Information Facility</a>. Data publishers retain all rights to data.</p>
+    <?php endif; ?>
     <?php
       print $content;
     ?>
---
+++ sites/all/themes/gbifwhite2012/theme-settings.php
@@ -0,0 +1,14 @@
+<?php
+
+function gbifwhite2012_form_system_theme_settings_alter(&$form, &$form_state) {
+  $form['theme_settings']['toggle_gbif_footer'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('GBIF links in the footer block'),
+    '#default_value' => theme_get_setting('toggle_gbif_footer'),
+  );
+  $form['theme_settings']['toggle_gbif_credit'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('GBIF credit in the footer block'),
+    '#default_value' => theme_get_setting('toggle_gbif_credit'),
+  );
+}
