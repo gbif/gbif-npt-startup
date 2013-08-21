84ab38e8df5d1effb2bfa5a191a9cbc4e756a2ae
BurkeChih-JenKo <bko@gbif.org>
2013-08-21 17:59:58 +0200

    Dynamic dropdown now works with RAW jQuery.

---
+++ profiles/npt_startup/js/listParticipants.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function() {
+
+  jQuery("#edit-participant-type").change(function() {
+
+    var nodeType = jQuery(this).val();
+    var list = Drupal.settings.npt_startup[nodeType];
+    var typeLabel = jQuery("#edit-participant-type option:selected").text();
+    var nodeSelect = jQuery('#edit-node-uuid');
+    
+    jQuery("label[for=edit-node-uuid]").text("GBIF " + typeLabel);
+    nodeSelect.empty();
+    jQuery.each(list, function(uuid, node) {
+      nodeSelect.append(jQuery("<option/>", {
+        value: uuid,
+        text: node
+      }));
+    });
+  });
+
+});
