2be35f2850a320da882445431673d9215c5048ff
BurkeChih-JenKo <bko@gbif.org>
2013-08-22 10:39:52 +0200

    Removed customised chosen by deims.

--- profiles/npt_startup/js/dropdown.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function() {
-  jQuery("#edit-dropdown-first").change(function() {
-     jQuery("#edit-dropdown-second").load("profiles/npt_startup/js/textdata/" + jQuery(this).val() + ".txt");
-  });  
-});
-
