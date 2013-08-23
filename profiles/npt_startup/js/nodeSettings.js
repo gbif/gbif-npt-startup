2d774a9ab70a1fd1cafd315935bfc50c16c8a86f
BurkeChih-JenKo <bko@gbif.org>
2013-08-23 18:27:52 +0200

    Applied settings to npt_startup module. Very ugly code.

---
+++ profiles/npt_startup/js/nodeSettings.js
@@ -0,0 +1,57 @@
+(function ($) {
+
+  Drupal.behaviors.npt_starup = {
+    attach: function (context, settings) {
+
+      $(document).ready(function() {
+
+        $("#edit-participant-type").change(function() {
+
+          var nodeType = $(this).val();
+          var list = Drupal.settings.npt_startup[nodeType];
+          var typeLabel = $("#edit-participant-type option:selected").text();
+          var nodeSelect = $('#edit-node-uuid');
+    
+          $("label[for=edit-node-uuid]").text("GBIF " + typeLabel);
+          nodeSelect.empty();
+          $.each(list, function(uuid, node) {
+            nodeSelect.append($("<option/>", {
+              value: uuid,
+              text: node
+            }));
+          });
+          nodeSelect.trigger("chosen:updated");
+        });
+
+        var lat = Drupal.settings.npt_startup.lat;
+        var lng = Drupal.settings.npt_startup.lng;
+        var map = L.map('node_map').setView([lat, lng], 6);
+
+        L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
+        	maxZoom: 18,
+        	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+        }).addTo(map);
+
+        L.marker([lat, lng])
+          .addTo(map).bindPopup("Current map centre.")
+          .openPopup();
+
+        var popup = L.popup();
+
+        function onMapClick(e) {
+        	popup
+        		.setLatLng(e.latlng)
+        		.setContent("Assigned new map centre at " + e.latlng.toString())
+        		.openOn(map);
+
+        	$("#edit-node-coordinate-lat").val(e.latlng.lat);
+        	$("#edit-node-coordinate-lng").val(e.latlng.lng);
+        }
+
+        map.on('click', onMapClick);
+
+      });
+    }
+  };
+
+})(jQuery);
--- profiles/npt_startup/npt_startup.profile
+++ profiles/npt_startup/npt_startup.profile
@@ -5,3 +5,52 @@
  * Instructing the installation and configuration so the Drupal site
  * is an instance of the NPT Startup
  */
+
+/**
+ * Implements hook_library().
+ */
+function npt_startup_library() {
+  $libraries['list_participants'] = array(
+    'title' => 'List Participants',
+    'js' => array(
+      drupal_get_path('profile', 'npt_startup') . '/js/listParticipants.js' => array(
+        'type' => 'file',
+        'scope' => 'footer',
+        'weight' => 20,
+      ),
+    ),
+  );
+  $libraries['node_coordinate'] = array(
+    'title' => 'Node Coordinate',
+    'js' => array(
+      drupal_get_path('module', 'npt_setup') . '/js/node_coordinate.js' => array(
+        'type' => 'file',
+        'scope' => 'footer',
+        'weight' => 20,
+      ),
+    ),
+    'css' => array(
+      drupal_get_path('module', 'npt_setup') . '/css/node_coordinate.css' => array(
+        'type' => 'file',
+        'media' => 'all',
+      ),
+    ),
+  );
+  $libraries['leaflet'] = array(
+    'title' => 'leaflet',
+    'js' => array(
+      libraries_get_path('leaflet') . '/leaflet.js' => array(
+        'type' => 'file',
+        'scope' => 'footer',
+        'weight' => 20,
+      ),
+    ),
+    'css' => array(
+      libraries_get_path('leaflet') . '/leaflet.css' => array(
+        'type' => 'file',
+        'media' => 'all',
+      ),
+    ),
+  );
+  return $libraries;
+}
