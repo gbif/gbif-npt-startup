cd35ba5239b4ca948f8b89e3d8475616ba7b84c9
BurkeChih-JenKo <bko@gbif.org>
2013-08-26 13:31:49 +0200

    Block of GBIF Participation.

--- sites/all/modules/custom/gbifs/npt_stats/js/gbifParticipation.js
+++ sites/all/modules/custom/gbifs/npt_stats/js/gbifParticipation.js
@@ -15,8 +15,27 @@
           contentType: "application/json",
           dataType: 'jsonp',
           success: function(json) {
+            
+            /*
+            if (typeof json.participationStatus != 'undefined') {
+              switch (json.participationStatus) {
+                case 'VOTING':
+                  var pageTitle = json.title + ' ' + Drupal.t('as a GBIF Voting Country Participant'); 
+                  $("#page-title").text(pageTitle);
+                  break;
+                case 'ASSOCIATE':
+                  var pageTitle = json.title + ' ' + Drupal.t('as a GBIF Associate Countrt Participant'); 
+                  $("#page-title").text(pageTitle);
+                  break;
+                case 'Other':
+                  var pageTitle = json.title + ' ' + Drupal.t('as a GBIF Associate Participant'); 
+                  $("#page-title").text(pageTitle);
+                  break;
+              }
+            }
+            */
               $("#block-npt-stats-gbif-participation").html(json.title);
-
+            
           },
           error: function(e) {
             console.log(e.message);
