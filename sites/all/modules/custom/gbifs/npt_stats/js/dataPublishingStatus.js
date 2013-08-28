5cec1d8c0f72149c6d8da93c8d2570fce7ec44bc
BurkeChih-JenKo <bko@gbif.org>
2013-08-28 11:53:25 +0200

    Link to the data portal.

--- sites/all/modules/custom/gbifs/npt_stats/js/dataPublishingStatus.js
+++ sites/all/modules/custom/gbifs/npt_stats/js/dataPublishingStatus.js
@@ -1,6 +1,9 @@
 var node_uuid = Drupal.settings.npt_stats.node_uuid;
+var nodeShortname = Drupal.settings.npt_stats.node_shortname + ' ';
 var enumName = Drupal.settings.npt_stats.node_country_enum;
+var iso2 = Drupal.settings.npt_stats.node_country;
 var api = Drupal.settings.npt_constants.GBIF_API;
+var portal = Drupal.settings.npt_constants.GBIF_PORTAL;
 var gbrdsAgent = Drupal.settings.npt_constants.GBRDS_AGENT_URL;
 var url = api + '/occurrence/count?hostCountry=' + enumName;
 var count, countGeo, percent;
@@ -16,12 +19,14 @@ var count, countGeo, percent;
       // call the second method
       $.getJSON(url + '&georeferenced=true&callback=?', function(data) {
         countGeo = data; 
-        percent = Math.round(countGeo / count * 100, -1);
+        percent = (countGeo / count * 100).toFixed(2);
 				
-				// now update the content on the page
-        $('<p/>', {
-          text: count + ' records published. ' + countGeo + ' (' + percent + '%) georeferenced.',
-        }).appendTo('#block-npt-stats-data-publishing-status div.content');
+				// prepare the link to the portal
+				var href = portal + '/country/' + iso2 + '/publishing';
+				var link = '<a href="' + href + '">More</a>';
+				var text = '<p>' + count + ' records published. ' + countGeo + ' (' + percent + '%) georeferenced. ' + link + ' on ' + nodeShortname + 'data publishing on the GBIF Portal.</p>';
+				
+				$('#block-npt-stats-data-publishing-status div.content').html(text);
       });
     });
   });
