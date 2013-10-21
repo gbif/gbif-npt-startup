330bdfb0d084c255e8016f2e7d3faf6da382ff40
BurkeChih-JenKo <bko@gbif.org>
2013-10-21 20:39:01 +0200

    Initial npt_map module... not working yet.

---
+++ sites/all/modules/custom/gbifs/npt_maps/js/npt_maps.js
@@ -0,0 +1,26 @@
+(function($){
+  Drupal.GM3.npt_country_mask = function(map){
+    gmap = map.google_map;
+
+    function initialize(){
+      var maskLayer = new google.maps.KmlLayer({
+        url: 'http://bkotemp.gbif.org/map/BEN_adm1.kmz'
+      });
+      maskLayer.setMap(gmap);
+    }
+
+    gmap.event.addDOMListener(window, 'load', initialize);
+
+    /* Add a toggle button.
+    $('#'+this.GM3.id).parent().append('<div class="gbif-toggle" style="position:relative; top:-20px;left:200px;height:0px;"><form><input id="gbif-toggle" type="checkbox" checked="checked"><label class="option" for="gbif-toggle">'+Drupal.t('GBIF data')+'</label></form></div>');
+    var self = this;
+    $('#gbif-toggle').change(function(){
+      if($('#gbif-toggle').attr('checked')){
+        self.GM3.google_map.overlayMapTypes.insertAt(0, self.overlay);        
+      } else {
+        self.GM3.google_map.overlayMapTypes.removeAt(0);  
+      }
+    });
+    */
+  }
+})(jQuery);
